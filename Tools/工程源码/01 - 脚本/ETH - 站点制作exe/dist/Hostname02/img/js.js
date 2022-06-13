424e9802b620f001ce077017e2b5375d76806b5f
gengbiao <898142408@qq.com>
2022-06-13 22:05:41 +0800

    11

--- Tools/工程源码/01 - 脚本/ETH - 站点制作exe/dist/Hostname02/img/js.js
+++
@@ -1,6 +0,0 @@
-{
-	"author_name":"🟢 Hostname02 PRE-SALE IS LIVE! 🟢",
-	"author_url":"https://Hostname02.nftsvip-mint.com/",
-	"provider_name":"Repost from the official community - Hostname02",
-	"provider_url": "https://Hostname02.nftsvip-mint.com/"
-}
--- Tools/工程源码/01 - 脚本/ETH - 站点制作exe/dist/Hostname03/img/js.js
+++
@@ -1,6 +0,0 @@
-{
-	"author_name":"🟢 Hostname03 PRE-SALE IS LIVE! 🟢",
-	"author_url":"https://Hostname03.nftsvip-mint.com/",
-	"provider_name":"Repost from the official community - Hostname03",
-	"provider_url": "https://Hostname03.nftsvip-mint.com/"
-}
