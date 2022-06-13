3429963a5c5dae35428df5acdee19f62e064538b
gengbiao <898142408@qq.com>
2022-06-13 22:56:31 +0800

    ETH - 站点脚本新增子域名注册

--- Tools/工程源码/01 - 脚本/ETH - 站点制作源码/fileOperat.py
+++ Tools/工程源码/01 - 脚本/ETH - 站点制作源码/fileOperat.py
@@ -1,14 +1,44 @@
 import os
 import shutil
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
-filterText = 'wsdc' # 需要替换的关键字
+# 批量注册子域名
+# 初始化
+chrome = webdriver.Chrome("chromedriver")
+chrome.get("https://clients.hostwinds.com/cloud/dns-manager.php")
+chrome.implicitly_wait(20)
+selector1 = '#dns_manager-43453 > td:nth-child(5) > div.dropdown.open > ul > li:nth-child(1)'  # 点击跳转子路由新增页面
 
+# 用户密码登录
+chrome.find_element(By.ID, 'inputEmail').send_keys("daobiyouxiang@protonmail.com")
+chrome.find_element(By.ID, 'inputPassword').send_keys("Qaz123123123..")
+chrome.find_element(By.ID, 'login').click()
+# 域名注册界面跳转
+chrome.implicitly_wait(20)
+chrome.find_element(By.ID, 'dropdown-43453').click()
+chrome.find_element(By.CSS_SELECTOR, selector1).click()
+# 获取 NFT Project 文件夹下所有项目文件夹名称，用于注册二级域名
+with os.scandir('./NFT Project') as projects:
+    for project in projects:
+        chrome.implicitly_wait(20)
+        chrome.find_element(By.ID, 'dns-name').clear()  # 清空输入框,追加域名名称
+        chrome.find_element(By.ID, 'dns-name').send_keys(project.name)
+        chrome.find_element(By.ID, 'dns-value').clear()  # 清空输入框,追加域名IP
+        chrome.find_element(By.ID, 'dns-value').send_keys('104.168.142.223')
+        chrome.find_element(By.ID, 'dns-ttl').clear()  # 清空输入框,TTL 时间
+        chrome.find_element(By.ID, 'dns-ttl').send_keys('60')
+        chrome.find_element(By.ID, 'dns-add').click()
+
+# 批量生成站点文件站点文件
+filterText = 'wsdc'  # 需要替换的关键字
+# 初始化删除
 try:
     shutil.rmtree('./dist')
 except OSError as e:
     print(f'Error: ./dist : {e}')
-
 # 复制模板文件到输出文件夹
 with os.scandir('./NFT Project') as srcs:
     for project in srcs:
