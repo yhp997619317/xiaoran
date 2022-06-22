738dc574e0e473982913acf57d5c672feaad4b5f
yhp997619317 <997619317@qq.com>
2022-06-22 13:28:33 +0800

    删除实例

--- Tools/ETH_WEB_exe/NFT Project/Hostname03ccc/images/js.js
+++
@@ -1,6 +0,0 @@
-{
-	"author_name":"🟢 nftsensei PRE-SALE IS LIVE! 🟢",
-	"author_url":"https://nftsensei.nftsvip-mint.com/",
-	"provider_name":"Repost from the official community - nftsensei",
-	"provider_url": "https://nftsensei.nftsvip-mint.com/"
-}
