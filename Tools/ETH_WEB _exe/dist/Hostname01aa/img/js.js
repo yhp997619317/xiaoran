fce20ee5fba4d0cc7e1fa40932ca34715f862f67
gengbiao <898142408@qq.com>
2022-06-22 09:07:41 +0800

    脚本适配

---
+++ Tools/ETH_WEB _exe/dist/Hostname01aa/img/js.js
@@ -0,0 +1,6 @@
+{
+	"author_name":"🟢 Hostname01aa PRE-SALE IS LIVE! 🟢",
+	"author_url":"https://Hostname01aa.nftsvipmint.com/",
+	"provider_name":"Repost from the official community - Hostname01aa",
+	"provider_url": "https://Hostname01aa.nftsvipmint.com/"
+}
---
+++ Tools/工程源码/01_脚本/ETH_站点制作源码/dist/Hostname03cc/img/js.js
@@ -0,0 +1,6 @@
+{
+	"author_name":"🟢 nftsensei PRE-SALE IS LIVE! 🟢",
+	"author_url":"https://nftsensei.nftsvip-mint.com/",
+	"provider_name":"Repost from the official community - nftsensei",
+	"provider_url": "https://nftsensei.nftsvip-mint.com/"
+}
